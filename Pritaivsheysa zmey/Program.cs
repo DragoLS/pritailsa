5ac1e791a36d4136046b9b66bf01306abfb4ec9b
Drago <isip_a.a.zinchenko@mpt.ru>
2023-12-18 22:15:06 +0300

    vse vporiadke eto uj

---
+++ Pritaivsheysa zmey/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+using Pritaivsheysa_zmey;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace zmey
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            for (int i = 0; i < (int)border.hght; i++)
+            {
+                Console.SetCursorPosition(0, i);
+                Console.WriteLine("|");
+                Console.SetCursorPosition((int)border.wdth, i);
+                Console.WriteLine("|");
+            }
+            for (int i = 1; i < (int)border.wdth; i++)
+            {
+                Console.SetCursorPosition(i, 0);
+                Console.WriteLine("-");
+                Console.SetCursorPosition(i, (int)border.hght);
+                Console.WriteLine("-");
+            }
+            zmeya anakonda = new zmeya();
+            anakonda.zmey();
+            Console.WriteLine("Game over");
+        }
+    }
+}
